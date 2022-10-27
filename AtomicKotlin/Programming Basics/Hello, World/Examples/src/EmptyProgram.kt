6e61c856b9240720718e2691b42065c429a19a57
道禹 Walter <sunwenhao.swh@alibaba-inc.com>
2022-10-27 17:12:59 +0800

    [+]add project

---
+++ AtomicKotlin/Programming Basics/Hello, World/Examples/src/EmptyProgram.kt
@@ -0,0 +1,6 @@
+// HelloWorld/EmptyProgram.kt
+
+fun main() {
+  // Program code here ...
+    print("Hello, World!")
+}
---
+++ AtomicKotlin/Programming Basics/Hello, World/Exercise 1/src/Task.kt
@@ -0,0 +1,7 @@
+// HelloWorld/Task1.kt
+package helloWorldExercise1
+
+// TODO
+fun main(){
+    print("Hello, Kotlin!")
+}
